4b7348e5d13a9d3b8ae044a34509431bdc72008a
Gonzalo Vega <gonzalovega7@gmail.com>
2022-10-23 19:42:04 -0300

    First commit - starting app

---
+++ app.js
@@ -0,0 +1,10 @@
+require('colors');
+
+const { showMenu } = require('./helpers/messages');
+
+
+const main = async() => {
+    showMenu();
+} 
+
+main();
---
+++ helpers/messages.js
@@ -0,0 +1,35 @@
+const { ModuleDetectionKind } = require('typescript');
+
+require('colors');
+
+const showMenu = () => {
+
+    console.clear();
+    console.log('====================='.green);
+    console.log('  Choose an option: '.green);
+    console.log('=====================\n'.green);
+
+    console.log(`${'1.'.green} Create task`);
+    console.log(`${'2.'.green} List tasks`);
+    console.log(`${'3.'.green} List complete tasks`);
+    console.log(`${'4.'.green} List pending tasks`);
+    console.log(`${'5.'.green} Complete task(s)`);
+    console.log(`${'6.'.green} Delete task`);
+    console.log(`${'0.'.green} Exit \n`);
+
+    const readline = require('readline').createInterface ({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    
+    readline.question('Choose an opction: ', (opt) => {
+        console.log({opt});
+        readline.close();
+    })
+}
+
+
+module.exports = {
+    showMenu
+}
